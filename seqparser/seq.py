c92afc830fbeadfbcf783491908ed2430ae827b9
Sebastian Cruz-Gonzalez <seba.2550@gmail.com>
2022-01-09 16:37:08 -0800

    Create transcribe and reverse transcribe functions

--- seqparser/seq.py
+++ seqparser/seq.py
@@ -6,10 +6,16 @@ def transcribe(seq: str) -> str:
     transcribes DNA to RNA by replacing
     all `T` to `U`
     """
+    rna = seq.replace('T','U')
 
+    return rna
 
 def reverse_transcribe(seq: str) -> str:
     """
     transcribes DNA to RNA by replacing
     all `T` to `U` then reverses the sequence
     """
+    rna = transcribe(seq)
+    reverse_rna = rna[::-1]
+
+    return reverse_rna
