158f591d76e903c7ad7f058d19ecee87a78e443f
Sebastian Cruz-Gonzalez <seba.2550@gmail.com>
2022-01-09 16:50:18 -0800

    Write unit tests for parsers

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -28,7 +28,17 @@ def test_FastaParser():
     your FastaParser class and assert that it properly
     reads in the example Fasta File.
     """
-    pass
+    fasta_path = "/Users/sebastiancruzgonzales/Documents/UCSF/BMI_203/project1/data/test.fa"
+    fasta_test_instance = FastaParser(fasta_path)
+
+    fasta_line_count = 0
+    for i in fasta_test_instance:
+        fasta_line_count += 2 # Count the two lines per record that we parsed from the fasta file
+
+
+    fasta_num_lines = sum(1 for line in open(fasta_path))
+
+    assert fasta_line_count == fasta_num_lines # Number of lines in file must match the number of records we parsed * 2
 
 
 def test_FastqParser():
@@ -38,4 +48,14 @@ def test_FastqParser():
     your FastqParser class and assert that it properly
     reads in the example Fastq File.
     """
-    pass
+    fastq_path = "/Users/sebastiancruzgonzales/Documents/UCSF/BMI_203/project1/data/test.fq"
+    fastq_test_instance = FastqParser(fastq_path)
+
+    fastq_line_count = 0
+    for i in fastq_test_instance:
+        fastq_line_count += 4 # Count the for lines per record that we parsed from the fastq file
+
+    fastq_num_lines = sum(1 for line in open(fastq_path))
+
+    assert fastq_line_count == fastq_num_lines # Number of lines in file must match the number of records we parsed * 4
+
